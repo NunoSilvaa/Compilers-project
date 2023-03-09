da64b00d13aa13d0c19910759fdc121a6ac4242a
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-03-09 11:17:06 +0000

    Added setters

--- src/main/pt/up/fe/comp2023/analysis/SymbolTableConstructor.java
+++ src/main/pt/up/fe/comp2023/analysis/SymbolTableConstructor.java
@@ -1,14 +1,36 @@
 package pt.up.fe.comp2023.analysis;
 
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
+import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp2023.analysis.table.ImplementedSymbolTable;
 
 import java.util.List;
 
-public class SymbolTableConstructor extends AJmmVisitor<SymbolTableConstructor, List<Report>> {
+public class SymbolTableConstructor extends AJmmVisitor<ImplementedSymbolTable, List<Report>> {
 
     @Override
     protected void buildVisitor() {
+        addVisit("Program", this::dealWithProgram);
+        addVisit("importDeclaration", this::dealWithImport);
+        addVisit("methodDeclaration", this::dealWithMethodDeclaration);
+        addVisit("classDeclaration", this::dealWithClassDeclaration);
+    }
 
+    private List<Report> dealWithProgram(JmmNode jmmNode, ImplementedSymbolTable implementedSymbolTable) {
+        return null;
+    }
+
+    private List<Report> dealWithImport(JmmNode jmmNode, ImplementedSymbolTable implementedSymbolTable) {
+        return null;
+    }
+
+    private List<Report> dealWithMethodDeclaration(JmmNode jmmNode, ImplementedSymbolTable implementedSymbolTable) {
+        return null;
+    }
+
+    private List<Report> dealWithClassDeclaration(JmmNode jmmNode, ImplementedSymbolTable implementedSymbolTable) {
+        List<Report> reports;
+        return null;
     }
 }
