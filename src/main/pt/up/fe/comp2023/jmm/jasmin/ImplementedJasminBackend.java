019222fceba075a00d890294b2fa82316f9b3cc2
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-05-18 11:55:49 +0100

    Test

--- src/main/pt/up/fe/comp2023/jmm/jasmin/ImplementedJasminBackend.java
+++ src/main/pt/up/fe/comp2023/jmm/jasmin/ImplementedJasminBackend.java
@@ -497,6 +497,7 @@ public class ImplementedJasminBackend implements JasminBackend {
                         else {
                             strInst.append(this.loadStack(left, varTable)).append(this.loadStack(right, varTable));
                             op = "if_icmplt";
+                            updateStackLimit(-1);
                         }
                     }
                     case ANDB -> {
