758710787dd1ce30f4efee64c37ac075aa1149c0
NunoSilvaa <nmls1892002@gmail.com>
2023-05-28 12:14:57 +0100

    revert

--- src/main/pt/up/fe/comp2023/AnalysisVisitor.java
+++ src/main/pt/up/fe/comp2023/AnalysisVisitor.java
@@ -477,7 +477,32 @@ public class AnalysisVisitor extends PreorderJmmVisitor<String, String> {
         if (!node.getJmmParent().getKind().equals("ExpressionStatement")) {
             if (symbolTable.getMethods().contains(node.get("methodCallName")) || symbolTable.getImports().contains(node.get("methodCallName"))) {
                 if (node.getChildren().size() - 1 == symbolTable.getParameters(node.get("methodCallName")).size()) {
-                    boolean next = true;
+                    for (Symbol localVariable : symbolTable.getLocalVariables(methodNodeName)) {
+                        if (node.getChildren().size() > 1) {
+                            for (int i = 1; i < node.getChildren().size(); i++) {
+                                if (localVariable.getName().equals(node.getChildren().get(i).get("value"))) {
+                                    for (int j = 0; j < symbolTable.getParameters(node.get("methodCallName")).size(); j++) {
+                                        if (i - 1 == j) {
+                                            if (!symbolTable.getParameters(node.get("methodCallName")).get(j).getType().equals(localVariable.getType())) {
+                                                reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colEnd")), "Incompatible arguments type!"));
+                                            }
+                                        }
+                                    }
+                            /*if (!symbolTable.getParameters(node.get("methodCallName")).contains(localVariable)) {
+                                var t = symbolTable.getParameters(node.get("methodCallName"));
+                                reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colEnd")), "Incompatible arguments type!"));
+                            }*/
+                                }
+                            }
+                        }
+                    /*if (localVariable.getName().equals(node.getChildren().get(1).get("value"))) {
+                        if (!symbolTable.getParameters(node.get("methodCallName")).contains(localVariable)) {
+                            var t = symbolTable.getParameters(node.get("methodCallName"));
+                            reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colEnd")), "Incompatible arguments type!"));
+                        }
+                    }*/
+                    }
+                    /*boolean next = true;
                     for (int i = 1; i < node.getChildren().size(); i++) {
                         if (node.getChildren().get(i).getKind().equals("This")) {
                             next = false;
@@ -500,7 +525,7 @@ public class AnalysisVisitor extends PreorderJmmVisitor<String, String> {
                             /*if (!symbolTable.getParameters(node.get("methodCallName")).contains(localVariable)) {
                                 var t = symbolTable.getParameters(node.get("methodCallName"));
                                 reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colEnd")), "Incompatible arguments type!"));
-                            }*/
+                            }
                                     }
                                 }
                             }
@@ -509,9 +534,9 @@ public class AnalysisVisitor extends PreorderJmmVisitor<String, String> {
                             var t = symbolTable.getParameters(node.get("methodCallName"));
                             reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colEnd")), "Incompatible arguments type!"));
                         }
-                    }*/
-                        }
                     }
+                        }
+                    }*/
                 } else {
                     reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colEnd")), "Method call has the wrong number of arguments!"));
                 }
